1c6e5dde93785d5cbd6807f13a13f50b278cad72
vjspranav <pranavsri@live.in>
2020-11-29 04:13:31 +0530

    Small Fixed * Add a README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kali type zsh config
+Just clone the repo
+```
+bash script.sh
+```
+and wait for it.  
+## TODO
+* Add color to script  
+* Properly add folder for highlights and suggestions  
+* Also add optional qterminal theme
--- script.sh
+++ script.sh
@@ -15,11 +15,11 @@
 man=0
 
 _installzsh() {
-  if [ $man = 1 ]; then sudo apt install zsh; fi
+  if [ $man = 1 ]; then sudo apt install -y zsh; fi
   if [ $man = 2 ]; then sudo pacman -Sy zsh; fi
   if [ $man = 3 ]; then sudo yum update && sudo yum -y install zsh; fi
   if PM="$( which zsh )" 2> /dev/null; then
-     echo "zsh already installed"
+     echo "zsh Installed Successfully"
   else
      echo "Installing zsh Failed Aborting"
      exit 1
@@ -28,20 +28,24 @@ _installzsh() {
 
 _checkIntegrity() {
    echo "Verifying sha256sum of dircolors"
+   sleep 1
    if [ $(sha256sum scripts/dircolors | cut -d ' ' -f 1) = "2e57a55cb059ee5535f1e4efcc3b1cd6f2ecc01a97326372126b7d98f343e624" ]; then
    echo "2e57a55cb059ee5535f1e4efcc3b1cd6f2ecc01a97326372126b7d98f343e624 Verified OK"
    else echo "2e57a55cb059ee5535f1e4efcc3b1cd6f2ecc01a97326372126b7d98f343e624 Failed"; exit 1
    fi
+   sleep 1
    echo "Verifying sha256sum of zsh-autosuggestions.zsh"
    if [ $(sha256sum scripts/zsh-autosuggestions.zsh | cut -d ' ' -f 1) = "7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e" ]; then
    echo "7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e Verified OK"
    else echo "7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e Failed"; exit 1
    fi
+   sleep 1
    echo "Verifying sha256sum of zsh-syntax-highlighting.zsh"
    if [ $(sha256sum scripts/zsh-syntax-highlighting.zsh | cut -d ' ' -f 1) = "b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884" ]; then
    echo "b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884 Verified OK"
    else echo "b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884 Failed"; exit 1
    fi
+   sleep 1
    echo "Verifying sha256sum of zshrc"
    if [ $(sha256sum scripts/zshrc | cut -d ' ' -f 1) = "095ed1ef56d4f644c2ebbac01e93763affba300c7605c2ae937e32139000adf8" ]; then
    echo "095ed1ef56d4f644c2ebbac01e93763affba300c7605c2ae937e32139000adf8 Verified OK"
@@ -71,27 +75,33 @@ else
    echo "Installed zsh successfully"
 fi
 chsh -s $(which zsh)
+sleep 1
 
 #Check integretiy of config files
 _checkIntegrity
 echo "Integrity check PASSED!"
+sleep 1
 
 echo "Backing Up Existing zsh config"
 cp ~/.zshrc ~/.zshrc_backup
 
+sleep 1
 echo "Copying Kali's config"
 cp scripts/zshrc ~/.zshrc
 
+sleep 1
 echo "Copying Auto Suggestions"
 sudo mkdir -p /usr/share/zsh-autosuggestions/
 sudo cp scripts/zsh-autosuggestions.zsh /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+sleep 1
 echo "Copying Highlighting script"
 sudo mkdir -p /usr/share/zsh-syntax-highlighting/
 sudo cp scripts/zsh-syntax-highlighting.zsh /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 sudo chmod 644 /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+sleep 1
 echo "Copying Dircolors"
 if [ -f /usr/bin/dircolors ]; then echo "Already Exists";
 else
@@ -104,4 +114,5 @@ fi
 
 sudo chmod +x /usr/bin/dircolors
 
+sleep 1
 echo "Kali zsh config successfully installed. Log out and login to see the changes"
