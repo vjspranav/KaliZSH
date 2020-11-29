f1b208b714abf0660e17813f7e8de47398671641
vjspranav <pranavasri@live.in>
2020-11-29 11:51:03 +0530

    Get highlighting and autosuggestoin directly from repo

--- script.sh
+++ script.sh
@@ -2,8 +2,6 @@
 
 # Sha256 sum of included files
 # 2e57a55cb059ee5535f1e4efcc3b1cd6f2ecc01a97326372126b7d98f343e624 dircolors
-# 7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e zsh-autosuggestions.zsh
-# b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884 zsh-syntax-highlighting.zsh
 # 095ed1ef56d4f644c2ebbac01e93763affba300c7605c2ae937e32139000adf8 zshrc
 
 # Variable that defines package manager
@@ -15,9 +13,9 @@
 man=0
 
 _installzsh() {
-  if [ $man = 1 ]; then sudo apt install -y zsh; fi
-  if [ $man = 2 ]; then sudo pacman -Sy zsh; fi
-  if [ $man = 3 ]; then sudo yum update && sudo yum -y install zsh; fi
+  if [ $man = 1 ]; then sudo apt install -y zsh git; fi
+  if [ $man = 2 ]; then sudo pacman -Sy zsh git; fi
+  if [ $man = 3 ]; then sudo yum update && sudo yum -y install zsh git; fi
   if PM="$( which zsh )" 2> /dev/null; then
      echo "zsh Installed Successfully"
   else
@@ -33,20 +31,8 @@ _checkIntegrity() {
    echo "2e57a55cb059ee5535f1e4efcc3b1cd6f2ecc01a97326372126b7d98f343e624 Verified OK"
    else echo "2e57a55cb059ee5535f1e4efcc3b1cd6f2ecc01a97326372126b7d98f343e624 Failed"; exit 1
    fi
-   sleep 1
-   echo "Verifying sha256sum of zsh-autosuggestions.zsh"
-   if [ $(sha256sum scripts/zsh-autosuggestions.zsh | cut -d ' ' -f 1) = "7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e" ]; then
-   echo "7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e Verified OK"
-   else echo "7f5d56a502671c18cdf8056239c67963afd58bd21c19717ec5921cdc314dc75e Failed"; exit 1
-   fi
-   sleep 1
-   echo "Verifying sha256sum of zsh-syntax-highlighting.zsh"
-   if [ $(sha256sum scripts/zsh-syntax-highlighting.zsh | cut -d ' ' -f 1) = "b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884" ]; then
-   echo "b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884 Verified OK"
-   else echo "b597811f7f6c1169d45d4820d3a3dcfc5053ceefb8f88c5b0f4562f500499884 Failed"; exit 1
-   fi
-   sleep 1
    echo "Verifying sha256sum of zshrc"
+   sleep 1
    if [ $(sha256sum scripts/zshrc | cut -d ' ' -f 1) = "095ed1ef56d4f644c2ebbac01e93763affba300c7605c2ae937e32139000adf8" ]; then
    echo "095ed1ef56d4f644c2ebbac01e93763affba300c7605c2ae937e32139000adf8 Verified OK"
    else echo "095ed1ef56d4f644c2ebbac01e93763affba300c7605c2ae937e32139000adf8 Failed"; exit 1
@@ -91,19 +77,18 @@ cp scripts/zshrc ~/.zshrc
 
 sleep 1
 echo "Copying Auto Suggestions"
-sudo mkdir -p /usr/share/zsh-autosuggestions/
-sudo cp scripts/zsh-autosuggestions.zsh /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+sudo mkdir -p /usr/share/
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh-autosuggestions/
 
 sleep 1
 echo "Copying Highlighting script"
-sudo mkdir -p /usr/share/zsh-syntax-highlighting/
-sudo cp scripts/zsh-syntax-highlighting.zsh /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+sudo git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh-syntax-highlighting/
 
 sudo chmod 644 /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 sleep 1
 echo "Copying Dircolors"
-if [ -f /usr/bin/dircolors ]; then echo "Already Exists";
+if [ -f /usr/bin/dircolors ]; then echo "Dircolors Already Exists";
 else
   if [ ! -d /usr/bin/ ]; then
     sudo mkdir -p /usr/bin
